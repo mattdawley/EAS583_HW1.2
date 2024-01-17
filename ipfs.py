8850c77a1708fa7bf3413b9c0f45bcbbf3317d7d
Matt Dawley <dawley@gmail.com>
2024-01-16 20:47:38 -0700

    get v2

--- ipfs.py
+++ ipfs.py
@@ -33,7 +33,7 @@ def get_from_ipfs(cid,content_type="json"):
 		"pinata_secret_api_key": "3b3a7b65f0d95d6efa153fc46578f966241c71f1f9f43c7d199c6d4d0d10c82a",
 	}
 	response = requests.get(pinata_gateway_url, headers=headers)
-	data = response.json()
+	data = json.loads(response.json())
 	assert isinstance(data,dict), f"get_from_ipfs should return a dict"
 	return data
 
