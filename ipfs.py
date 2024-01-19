841120c4b913d2906f9dde8aeaab7c3567a8a320
Matt Dawley <dawley@gmail.com>
2024-01-19 08:03:59 -0700

    revised version 3

--- ipfs.py
+++ ipfs.py
@@ -4,37 +4,36 @@ import json
 def pin_to_ipfs(data):
 	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
 
+	# Convert dict to json
+	json_data = json.dumps(data)
+	payload = {"pinataContent": json_data }
+
+	# Set parameters for requests.post
 	pinata_api_url = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
 	headers = {
+		"content-type": "application/json",
 		"pinata_api_key": "4e8b2b5c83d672a0f9af",
 		"pinata_secret_api_key": "3b3a7b65f0d95d6efa153fc46578f966241c71f1f9f43c7d199c6d4d0d10c82a",
 	}
 
-	json_data = json.dumps(data)
-
-	payload = { "content-type": "application/json", "pinataContent": json_data }
-
+	# Post data
 	response = requests.post(pinata_api_url, json=payload, headers=headers)
 
 	# Check if the request was successful (status code 200)
 	if response.status_code == 200:
 		cid = response.json()["IpfsHash"]
 	else:
-		cid = "na"
+		cid = None
 
 	return cid
 
 def get_from_ipfs(cid,content_type="json"):
 	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
 
-	# Updated Gateway
+	# Set parameters for requests.get
 	pinata_gateway_url = "https://gateway.pinata.cloud/ipfs/"+cid
-
-	headers = {
-		"pinata_api_key": "4e8b2b5c83d672a0f9af",
-		"pinata_secret_api_key": "3b3a7b65f0d95d6efa153fc46578f966241c71f1f9f43c7d199c6d4d0d10c82a",
-	}
-	response = requests.get(pinata_gateway_url, headers=headers)
+	headers = {"content_type": content_type}
+	response = requests.get(pinata_gateway_url, headers)
 
 	if isinstance(response.json(), dict):
 		data = response.json()
